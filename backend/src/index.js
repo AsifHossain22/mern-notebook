31b9ca2aa4480c03c88297bc13c4dc211f56d7ac
AsifHossain22 <only1asif4you22@gmail.com>
2026-01-15 14:41:05 +0400

    Optimizing File & Folder Structure

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+// const express = require("express");
+import express from "express";
+
+import noteRoutes from "./routes/notesRoutes.js";
+
+const app = express();
+
+app.use("/api/notes", noteRoutes);
+
+app.listen(5001, () => {
+  console.log("Server running on PORT: 5001");
+});
