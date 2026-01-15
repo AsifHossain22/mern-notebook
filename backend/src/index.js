a511963858cb5620e7787756c0be7e4e5fa0206a
AsifHossain22 <only1asif4you22@gmail.com>
2026-01-15 16:24:59 +0400

    Setting Up Database (MongoDB)

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,18 @@
 import express from "express";
 
 import noteRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", noteRoutes);
 
-app.listen(5001, () => {
-  console.log("Server running on PORT: 5001");
+app.listen(PORT, () => {
+  console.log("Server running on PORT:", PORT);
 });
