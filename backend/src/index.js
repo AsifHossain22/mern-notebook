ea3f7ea6c1a7813632422be12993c64750a2df6a
AsifHossain22 <only1asif4you22@gmail.com>
2026-01-17 12:29:42 +0400

    Middleware & Rate Limiting

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,19 +4,27 @@ import express from "express";
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // Middleware
-app.use(express.json());
+app.use(express.json()); // ToParseJSONBodies
+app.use(rateLimiter);
+
+// SimpleMiddleware
+// app.use((req, res, next) => {
+//   console.log(`Request Method: ${req.method} & Request URL: ${req.url}`);
+//   next();
+// });
 
 app.use("/api/notes", noteRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server running on PORT:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on PORT:", PORT);
+  });
 });
