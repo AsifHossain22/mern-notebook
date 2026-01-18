e2e60507f465e24b6405f1c410e8efb697c0696d
AsifHossain22 <only1asif4you22@gmail.com>
2026-01-18 11:46:10 +0400

    Created Home Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
 
 const HomePage = () => {
+  // StateOfLimitedRate
+  const [isRateLimited, setIsRateLimited] = useState(false);
+
+  // StateOfNotes
+  const [notes, setNotes] = useState([]);
+
+  // StateOfLoading
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Error fetching notes", error);
+        console.log(error);
+
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    getNotes();
+  }, []);
+
   return (
     <div className="min-h-screen">
       <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note, i) => (
+              <div key={i}>
+                <NoteCard key={note._id} note={note} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
